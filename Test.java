3106098aaa9d51289fb9b02b1feaeee10c5b3be3
Kyerann Cochaux <kyerann.cochaux@gmail.com>
2026-09-23 11:06:27 +0200

    Making the tests more easier to do

--- Test.java
+++ Test.java
@@ -1,10 +1,64 @@
+import java.util.concurrent.Flow.Subscriber;
+
 public class Test
 {
-	public static void main(String[] arg)
+	public static void main(String[] args)
 	{
-		Maze maze = new Maze(5, 5);
+		Maze maze = new Maze();
+		
+		boolean bValidArgs = true;
+		
+		for (int i = 0; i < args.length; i++)
+			bValidArgs &= Test.isInt(args[i]);
+		
+		if ( !bValidArgs )
+		{
+			System.out.println("Invalid arguments !");
+			return;
+		}
+		
+		switch (args.length)
+		{
+			case 2:
+				maze = new Maze(
+					Integer.parseInt(args[0]), // Heigth
+					Integer.parseInt(args[1])  // Width
+				);
+				break;
+			
+			
+			case 6:
+				maze = new Maze(
+					Integer.parseInt(args[0]), // Height
+					Integer.parseInt(args[1]), // Width
+					
+					Integer.parseInt(args[2]), // Start X
+					Integer.parseInt(args[3]), // Start Y
+					
+					Integer.parseInt(args[4]), // End   X
+					Integer.parseInt(args[5])  // End   Y
+				);
+				break;
+			
+			default :
+				System.out.println("Generating the \"Basic\" Maze.");
+				break;
+		}
 		
 		System.out.println(maze);
 	}
 	
+	private static boolean isInt(String str)
+	{
+		try
+		{
+			Integer.parseInt(str);
+		}
+		catch (Exception e)
+		{
+			return false;
+		}
+		
+		return true;
+	}
 }
